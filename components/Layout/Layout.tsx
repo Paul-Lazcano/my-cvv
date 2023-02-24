5b4db10e755cb4e94f8c4696eba9be867425e7e6
Paul Lazcano <alwaysbetterpaul@gmail.com>
2023-02-24 15:20:04 -0700

    tailwind added

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { FC, ReactNode } from "react";
+import s from "./Layout.module.css";
+
+interface Props {
+  metadata: {
+    title: string;
+    description?: string;
+  };
+  children: ReactNode;
+}
+
+const Layout: FC<Props> = ({ metadata, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        {metadata?.description && (
+          <meta name="description" content={metadata.description} />
+        )}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={s.main}>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
